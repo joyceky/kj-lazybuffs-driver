6e4bbb2787ffc9834c95b81632cbc901f62f217a
KaneLabs <ryankane28@gmail.com>
2017-03-08 17:09:56 -0700

    active route signal

--- src/components/Driver/Active/Active.jsx
+++ src/components/Driver/Active/Active.jsx
@@ -6,17 +6,18 @@ class Active extends Component {
 
   mapListItems(orders) {
     return (
-      <ul>
+      <ul style={{ listStyle: 'none', width: '100%', padding: '0' }}>
         {orders.map((order) => {
-          <ListItemActive order={order} key={order.orderId} />
+          return <ListItemActive order={order} key={order.orderId} />
         })}
       </ul>
     );
   }
 
   render() {
+    console.log(this.props.driverActiveOrders);
       return (
-        this.props.driverActiveOrders[0]
+        this.props.driverActiveOrders.length > 0
         ? this.mapListItems(this.props.driverActiveOrders)
         : <h1>No Active Orders</h1>
       )
